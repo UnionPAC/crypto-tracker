5ec64572211f9fbeeb41ce6c75f534cb77d3c1a9
Geoff Jamieson <geoffjamieson@Geoffs-MacBook-Pro.local>
2022-12-01 17:23:24 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const fetch = require("node-fetch");
+require("dotenv").config();
+const app = express();
+const port = 5000;
+
+const COINMARKETCAP_SANDBOX_URL = process.env.COINMARKETCAP_SANDBOX_URL;
+const COINMARKETCAP_SANDBOX_API_KEY = process.env.COINMARKETCAP_SANDBOX_API_KEY;
+const COINMARKETCAP_URL = process.env.COINMARKETCAP_URL;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+
+app.get("/hi", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/api", async (req, res) => {
+  let fetch_response = await fetch(COINMARKETCAP_SANDBOX_URL, {
+    method: "GET",
+    headers: {
+      // note: fake API key for sandbox
+      "X-CMC_PRO_API_KEY": COINMARKETCAP_SANDBOX_API_KEY,
+    },
+  });
+  const json = await fetch_response.json();
+  res.json(json);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
