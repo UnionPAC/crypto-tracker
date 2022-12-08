871778e07558c3022c9d392c4dff1114382cfd96
Geoff Jamieson <geoffjamieson@Geoffs-MacBook-Pro.local>
2022-12-08 18:22:13 -0500

    ready for deploy

--- server.js
+++ server.js
@@ -3,18 +3,19 @@ const fetch = require("node-fetch");
 require("dotenv").config();
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 const COINMARKETCAP_SANDBOX_URL = process.env.COINMARKETCAP_SANDBOX_URL;
 const COINMARKETCAP_SANDBOX_API_KEY = process.env.COINMARKETCAP_SANDBOX_API_KEY;
 const COINMARKETCAP_URL = process.env.COINMARKETCAP_URL;
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-
-app.get("/", (req, res) => {
-  res.send("Route root");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+  });
+}
 
 app.get("/api", async (req, res) => {
   let fetch_response = await fetch(COINMARKETCAP_URL, {
@@ -28,6 +29,7 @@ app.get("/api", async (req, res) => {
   res.json(json);
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`App listening on port ${PORT}`);
 });
