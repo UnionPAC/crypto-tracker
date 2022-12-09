40976c94ed52f65905b3a9271c733af5be3e7c06
Geoff Jamieson <geoffjamieson@Geoffs-MacBook-Pro.local>
2022-12-08 19:01:40 -0500

    still trying to fix static build issue

--- server.js
+++ server.js
@@ -3,15 +3,29 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
+const COINMARKETCAP_SANDBOX_URL = process.env.COINMARKETCAP_SANDBOX_URL;
+const COINMARKETCAP_SANDBOX_API_KEY = process.env.COINMARKETCAP_SANDBOX_API_KEY;
+const COINMARKETCAP_URL = process.env.COINMARKETCAP_URL;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+
 app.use(express.json({ extended: false }));
 
-// Routes
-app.use("/api", require("./routes/cmc_api"));
+router.get("/api", async (req, res) => {
+  let fetch_response = await fetch(COINMARKETCAP_URL, {
+    method: "GET",
+    headers: {
+      // note: fake API key for sandbox
+      "X-CMC_PRO_API_KEY": COINMARKETCAP_API_KEY,
+    },
+  });
+  const json = await fetch_response.json();
+  res.json(json);
+});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
