dde73068e14491178daec061f1b227bc76a2f835
Geoff Jamieson <geoffjamieson@Geoffs-MacBook-Pro.local>
2022-12-08 19:11:04 -0500

    something else

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const fetch = require("node-fetch");
+require("dotenv").config();
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
