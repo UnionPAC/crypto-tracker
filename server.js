3acb3cfa232e1fd02baa92ea19f1891847ac04bb
geoff <geoff.ijamieson@gmail.com>
2023-01-24 16:29:33 -0500

    deploy ready

--- server.js
+++ server.js
@@ -22,12 +22,10 @@ app.get("/api", async (req, res) => {
   res.json(json);
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
-  });
-}
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("client", "build", "index.html"));
+});
 
 app.listen(PORT, (err) => {
   if (err) throw err;
